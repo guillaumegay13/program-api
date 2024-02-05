6c84386bead8a5d23649ec5b6a993062887d2bf3
Guillaume Gay <ggay@dalet.com>
2024-02-05 07:43:37 +0100

    merging

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from workflow import Workflow
+from api import TrainProgramApi
 from langchain_community.chat_models import ChatOpenAI
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+from mangum import Mangum
 
 gptJsonModel = ChatOpenAI(
     #models : https://platform.openai.com/docs/models/gpt-3-5
@@ -17,7 +18,7 @@ gptJsonModel = ChatOpenAI(
 )
 
 # Define a Pydantic model for the JSON data input
-class ProgramInput(BaseModel):
+class UserInput(BaseModel):
     type: str
     gender: str
     level: str
@@ -27,19 +28,50 @@ class ProgramInput(BaseModel):
     weight: int
     age: int
 
+# Need all user input plus evidences as dict
+class MethodsInput(UserInput):
+    evidences: dict
+
+# Need all user input plus methods as dict
+class ProgramInput(UserInput):
+    methods: dict
+
 app = FastAPI()
 
-@app.post("/api/program/")
-async def create_program(input_data: ProgramInput):
-    result = Workflow(gptJsonModel, input_data.dict())
-    return result
+@app.post("/api/provide_evidences/")
+async def provide_evidences(input_data: UserInput):
+    trainProgramApi = TrainProgramApi(gptJsonModel, input_data.dict())
+    evidences = trainProgramApi.provide_evidences()
+    return evidences
 
+@app.post("/api/generate_methods/")
+async def generate_methods(input_data: MethodsInput):
+    trainProgramApi = TrainProgramApi(gptJsonModel, input_data.dict())
+    evidences = trainProgramApi.provide_evidences()
+    methods = trainProgramApi.generate_methods(evidences)
+    return methods
+
+@app.post("/api/generate_program/")
+async def generate_program(input_data: ProgramInput):
+    trainProgramApi = TrainProgramApi(gptJsonModel, input_data.dict())
+    evidences = trainProgramApi.provide_evidences()
+    methods = trainProgramApi.generate_methods(evidences)
+    program = trainProgramApi.generate_program(methods)
+    return program
+
+# For the full workflow, only the User Input is needed
+@app.post("/api/generate_train_program/")
+async def generate_train_program(input_data: UserInput):
+    trainProgramApi = TrainProgramApi(gptJsonModel, input_data.dict())
+    # Run the end-to-end workflow
+    # TODO : write the workflow code below instead of a class method
+    trainProgramApi.run_workflow()
+
+# Ping GET endpoint for testing purposes
 @app.get("/api/ping")
 def ping():
     return "Hello!"
 
-def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
 if __name__ == "__main__":
-    main()
+    handler = Mangum(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
